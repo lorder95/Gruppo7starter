727e41b13bf5a5caa7ac8454c2b5d9a6f3f68387
lorder95 <l.derosa23@studenti.unisa.it>
2017-10-31 14:03:19 +0100

    StarterProject
    Questa versione contiene lo StarterProject scaricato dal sito
    più il tutorial Player movement fino al paragrafo 3.2  (3.2 escluso)

---
+++ workers/unity/Assets/Gamelogic/EntityTemplates/EntityTemplateFactory.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Gamelogic.Core;
+using Improbable;
+using Improbable.Core;
+using Improbable.Player;
+using Improbable.Unity.Core.Acls;
+using Improbable.Worker;
+using Quaternion = UnityEngine.Quaternion;
+using UnityEngine;
+using Improbable.Unity.Entity;
+
+namespace Assets.Gamelogic.EntityTemplates
+{
+    public class EntityTemplateFactory : MonoBehaviour
+    {
+        public static Entity CreatePlayerCreatorTemplate()
+        {
+            var playerCreatorEntityTemplate = EntityBuilder.Begin()
+				.AddPositionComponent(new Improbable.Coordinates(0, SimulationSettings.PlayerSpawnHeight, 0).ToUnityVector(), CommonRequirementSets.PhysicsOnly)
+                .AddMetadataComponent(entityType: SimulationSettings.PlayerCreatorPrefabName)
+                .SetPersistence(true)
+                .SetReadAcl(CommonRequirementSets.PhysicsOrVisual)
+                .AddComponent(new Rotation.Data(Quaternion.identity.ToNativeQuaternion()), CommonRequirementSets.PhysicsOnly)
+                .AddComponent(new PlayerCreation.Data(), CommonRequirementSets.PhysicsOnly)
+			//	.AddComponent(new PlayerInput.Data(new Joystick(0, 0)), CommonRequirementSets.SpecificClientOnly(clientId))
+                .Build();
+
+            return playerCreatorEntityTemplate;
+        }
+
+        public static Entity CreatePlayerTemplate(string clientId)
+        {
+            var playerTemplate = EntityBuilder.Begin()
+
+				.AddPositionComponent(new Improbable.Coordinates(0, SimulationSettings.PlayerSpawnHeight, 0).ToUnityVector(), CommonRequirementSets.PhysicsOnly)
+			
+                .AddMetadataComponent(entityType: SimulationSettings.PlayerPrefabName)
+                .SetPersistence(false)
+                .SetReadAcl(CommonRequirementSets.PhysicsOrVisual)
+                .AddComponent(new Rotation.Data(Quaternion.identity.ToNativeQuaternion()), CommonRequirementSets.PhysicsOnly)
+                .AddComponent(new ClientAuthorityCheck.Data(), CommonRequirementSets.SpecificClientOnly(clientId))
+                .AddComponent(new ClientConnection.Data(SimulationSettings.TotalHeartbeatsBeforeTimeout), CommonRequirementSets.PhysicsOnly)
+				.AddComponent(new PlayerInput.Data(new Joystick(xAxis: 0, yAxis: 0)), CommonRequirementSets.SpecificClientOnly(clientId))
+                .Build();
+
+            return playerTemplate;
+        }
+
+        public static Entity CreateCubeTemplate()
+        {
+            var cubeTemplate = EntityBuilder.Begin()
+                .AddPositionComponent(Improbable.Coordinates.ZERO.ToUnityVector(), CommonRequirementSets.PhysicsOnly)
+                .AddMetadataComponent(entityType: SimulationSettings.CubePrefabName)
+                .SetPersistence(true)
+                .SetReadAcl(CommonRequirementSets.PhysicsOrVisual)
+                .AddComponent(new Rotation.Data(Quaternion.identity.ToNativeQuaternion()), CommonRequirementSets.PhysicsOnly)
+                .Build();
+
+            return cubeTemplate;
+        }
+    }
+}

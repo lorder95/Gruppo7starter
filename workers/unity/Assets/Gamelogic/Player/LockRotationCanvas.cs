f9ca01658d6d51d1f2c363a7c356483f6d3ea2af
ftrunfio <f.trunfio@studenti.unisa.it>
2017-11-25 15:57:46 +0100

    Fix name v2

---
+++ workers/unity/Assets/Gamelogic/Player/LockRotationCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockRotationCanvas : MonoBehaviour {
+    
+    Vector3 iniRot= new Vector3();
+	// Use this for initialization
+	void Start () {
+        iniRot = transform.eulerAngles;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.eulerAngles = iniRot;
+    }
+}
